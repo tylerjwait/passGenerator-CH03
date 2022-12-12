d6e9cfc7ab32606505ddc45ccb728f4fe1d3bd1e
tylerjwait <118226586+tylerjwait@users.noreply.github.com>
2022-12-12 14:36:26 -0500

    added variables and arrays to js file

--- script.js
+++ script.js
@@ -13,3 +13,46 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  // once you press the generate button a pop up shows up to ask you how long you want your password to be.
+  var  passwordLength = prompt("How long do you want your password to be? (between 8-128 characters");
+
+  // Run this code if all criteria for  password length is met
+  if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128);
+
+   // Below are items allowed to be in the password using an array
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var special   = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+    var number    = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    // assign command to pull from any of the variables defined above
+    var passwordSet = [];
+    var password = [];
+
+    // include special characters or not to include special characters
+    var chooseSpecial = confirm("Include special characters? Cancel / Ok")
+    if (chooseSpecial == true) {
+      passwordSet = passwordSet.concat(special)
+    };
+
+    // include number characters or not to include number characters
+    var chooseNumber = confirm("Include number characters? Cancel / Ok")
+    if (chooseNumber == true) {
+      passwordSet = passwordSet.concat(number)
+    };
+
+    // include UPPER CASE characters or not to include UPPER CASE characters
+    var chooseupperCase = confirm("Include UPPER CASE characters? Cancel / Ok")
+    if (chooseupperCase == true) {
+      passwordSet = passwordSet.concat(upperCase)
+    };
+
+    // include lower case characters or not to include lower case characters
+    var chooselowerCase = confirm("Include lower case characters? Cancel / Ok")
+    if (chooselowerCase == true) {
+      passwordSet = passwordSet.concat(lowerCase)
+    };
+
+    }
+
